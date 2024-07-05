2eb8d5d2698114ab1b87a4eb4f19c2142c166e49
Minh Nguyễn <nguyenminh168203@gmail.com>
2024-07-06 00:54:44 +0700

    update

---
+++ 5739_NguyenHoangMinh/frm_KhachHang.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace _5739_NguyenHoangMinh
+{
+    partial class frm_KhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_DiaChi = new System.Windows.Forms.TextBox();
+            this.txt_HoTen = new System.Windows.Forms.TextBox();
+            this.txt_MSKH = new System.Windows.Forms.TextBox();
+            this.lbl_DiaChi = new System.Windows.Forms.Label();
+            this.lbl_HoTen = new System.Windows.Forms.Label();
+            this.lbl_MSKH = new System.Windows.Forms.Label();
+            this.lbl_DSSV = new System.Windows.Forms.Label();
+            this.btn_Dong = new System.Windows.Forms.Button();
+            this.dt_GridKH = new System.Windows.Forms.DataGridView();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_GridKH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_DiaChi
+            // 
+            this.txt_DiaChi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_DiaChi.Location = new System.Drawing.Point(242, 149);
+            this.txt_DiaChi.Name = "txt_DiaChi";
+            this.txt_DiaChi.Size = new System.Drawing.Size(318, 30);
+            this.txt_DiaChi.TabIndex = 23;
+            // 
+            // txt_HoTen
+            // 
+            this.txt_HoTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_HoTen.Location = new System.Drawing.Point(242, 93);
+            this.txt_HoTen.Name = "txt_HoTen";
+            this.txt_HoTen.Size = new System.Drawing.Size(318, 30);
+            this.txt_HoTen.TabIndex = 22;
+            // 
+            // txt_MSKH
+            // 
+            this.txt_MSKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MSKH.Location = new System.Drawing.Point(242, 32);
+            this.txt_MSKH.Name = "txt_MSKH";
+            this.txt_MSKH.Size = new System.Drawing.Size(318, 30);
+            this.txt_MSKH.TabIndex = 21;
+            // 
+            // lbl_DiaChi
+            // 
+            this.lbl_DiaChi.AutoSize = true;
+            this.lbl_DiaChi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DiaChi.Location = new System.Drawing.Point(112, 152);
+            this.lbl_DiaChi.Name = "lbl_DiaChi";
+            this.lbl_DiaChi.Size = new System.Drawing.Size(72, 22);
+            this.lbl_DiaChi.TabIndex = 20;
+            this.lbl_DiaChi.Text = "Địa Chỉ";
+            // 
+            // lbl_HoTen
+            // 
+            this.lbl_HoTen.AutoSize = true;
+            this.lbl_HoTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_HoTen.Location = new System.Drawing.Point(112, 96);
+            this.lbl_HoTen.Name = "lbl_HoTen";
+            this.lbl_HoTen.Size = new System.Drawing.Size(69, 22);
+            this.lbl_HoTen.TabIndex = 19;
+            this.lbl_HoTen.Text = "Họ Tên";
+            // 
+            // lbl_MSKH
+            // 
+            this.lbl_MSKH.AutoSize = true;
+            this.lbl_MSKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MSKH.Location = new System.Drawing.Point(112, 35);
+            this.lbl_MSKH.Name = "lbl_MSKH";
+            this.lbl_MSKH.Size = new System.Drawing.Size(66, 22);
+            this.lbl_MSKH.TabIndex = 18;
+            this.lbl_MSKH.Text = "MSKH";
+            // 
+            // lbl_DSSV
+            // 
+            this.lbl_DSSV.AutoSize = true;
+            this.lbl_DSSV.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DSSV.Location = new System.Drawing.Point(69, 238);
+            this.lbl_DSSV.Name = "lbl_DSSV";
+            this.lbl_DSSV.Size = new System.Drawing.Size(196, 23);
+            this.lbl_DSSV.TabIndex = 31;
+            this.lbl_DSSV.Text = "Danh sách khách hàng";
+            // 
+            // btn_Dong
+            // 
+            this.btn_Dong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Dong.Location = new System.Drawing.Point(577, 195);
+            this.btn_Dong.Name = "btn_Dong";
+            this.btn_Dong.Size = new System.Drawing.Size(75, 30);
+            this.btn_Dong.TabIndex = 30;
+            this.btn_Dong.Text = "Đóng";
+            this.btn_Dong.UseVisualStyleBackColor = true;
+            this.btn_Dong.Click += new System.EventHandler(this.btn_Dong_Click);
+            // 
+            // dt_GridKH
+            // 
+            this.dt_GridKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt_GridKH.Location = new System.Drawing.Point(98, 274);
+            this.dt_GridKH.Name = "dt_GridKH";
+            this.dt_GridKH.RowHeadersWidth = 51;
+            this.dt_GridKH.RowTemplate.Height = 24;
+            this.dt_GridKH.Size = new System.Drawing.Size(554, 222);
+            this.dt_GridKH.TabIndex = 29;
+            this.dt_GridKH.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dt_GridKH_CellClick);
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Xoa.Location = new System.Drawing.Point(420, 195);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(75, 30);
+            this.btn_Xoa.TabIndex = 28;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            this.btn_Xoa.Click += new System.EventHandler(this.btn_Xoa_Click);
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sua.Location = new System.Drawing.Point(268, 195);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(75, 30);
+            this.btn_Sua.TabIndex = 27;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            this.btn_Sua.Click += new System.EventHandler(this.btn_Sua_Click);
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Them.Location = new System.Drawing.Point(98, 195);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(75, 30);
+            this.btn_Them.TabIndex = 26;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.Click += new System.EventHandler(this.btn_Them_Click);
+            // 
+            // frm_KhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.lbl_DSSV);
+            this.Controls.Add(this.btn_Dong);
+            this.Controls.Add(this.dt_GridKH);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.txt_DiaChi);
+            this.Controls.Add(this.txt_HoTen);
+            this.Controls.Add(this.txt_MSKH);
+            this.Controls.Add(this.lbl_DiaChi);
+            this.Controls.Add(this.lbl_HoTen);
+            this.Controls.Add(this.lbl_MSKH);
+            this.Name = "frm_KhachHang";
+            this.Text = "Quản Lý Khách Hàng";
+            this.Load += new System.EventHandler(this.frm_KhachHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dt_GridKH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_DiaChi;
+        private System.Windows.Forms.TextBox txt_HoTen;
+        private System.Windows.Forms.TextBox txt_MSKH;
+        private System.Windows.Forms.Label lbl_DiaChi;
+        private System.Windows.Forms.Label lbl_HoTen;
+        private System.Windows.Forms.Label lbl_MSKH;
+        private System.Windows.Forms.Label lbl_DSSV;
+        private System.Windows.Forms.Button btn_Dong;
+        private System.Windows.Forms.DataGridView dt_GridKH;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Them;
+    }
+}
+
---
+++ 5739_NguyenHoangMinh/frm_KhachHang.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5739_NguyenHoangMinh
+{
+    public partial class frm_KhachHang : Form
+    {
+        public frm_KhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Dong_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog;
+            dialog = MessageBox.Show("Bạn có muốn thoát không ?", "Cảnh báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            string chuoikn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""F:\Học Tập\Lập Trình Ứng Dụng .NET\5739_NguyenHoangMinh\5739_NguyenHoangMinh\5739_NguyenHoangMinh.mdf"";Integrated Security=True";
+            SqlConnection conn = new SqlConnection(chuoikn);
+            string sql = "Insert into KhachHang values ('" + txt_MSKH.Text + "',N'" + txt_HoTen.Text + "',N'" + txt_DiaChi.Text + "')";
+            SqlCommand comm = new SqlCommand(sql, conn);
+            conn.Open();
+            int kq = comm.ExecuteNonQuery();
+            conn.Close();
+            if (kq >= 1) MessageBox.Show("Thêm khách hàng thành công");
+            else MessageBox.Show("Thêm khách hàng thất bại");
+            load_kh();
+
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            string chuoikn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""F:\Học Tập\Lập Trình Ứng Dụng .NET\5739_NguyenHoangMinh\5739_NguyenHoangMinh\5739_NguyenHoangMinh.mdf"";Integrated Security=True";
+            SqlConnection conn = new SqlConnection(chuoikn);
+            string sql = "UPDATE KhachHang SET HoTen = '" + txt_HoTen.Text + "', DiaChi = '" + txt_DiaChi.Text + "' WHERE MSKH = '" + txt_MSKH.Text + "'";
+            SqlCommand comm = new SqlCommand(sql, conn);
+            conn.Open();
+            int kq = comm.ExecuteNonQuery();
+            conn.Close();
+            if (kq >= 1) MessageBox.Show("Sửa khách hàng thành công");
+            else MessageBox.Show("Sửa khách hàng thất bại");
+            load_kh();
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            string chuoikn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""F:\Học Tập\Lập Trình Ứng Dụng .NET\5739_NguyenHoangMinh\5739_NguyenHoangMinh\5739_NguyenHoangMinh.mdf"";Integrated Security=True";
+            SqlConnection conn = new SqlConnection(chuoikn);
+            string sql = "DELETE FROM KhachHang WHERE MSKH = '" + txt_MSKH.Text + "'";
+            SqlCommand comm = new SqlCommand(sql, conn);
+            conn.Open();
+            int kq = comm.ExecuteNonQuery();
+            conn.Close();
+            if (kq >= 1) MessageBox.Show("Xóa khách hàng thành công");
+            else MessageBox.Show("Xóa khách hàng thất bại");
+            load_kh();
+        }
+
+        public void load_kh()
+        {
+            string chuoikn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""F:\Học Tập\Lập Trình Ứng Dụng .NET\5739_NguyenHoangMinh\5739_NguyenHoangMinh\5739_NguyenHoangMinh.mdf"";Integrated Security=True";
+            string sql = "Select * From KhachHang";
+            SqlDataAdapter da = new SqlDataAdapter(sql, chuoikn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dt_GridKH.DataSource = dt;
+        }
+
+        private void frm_KhachHang_Load(object sender, EventArgs e)
+        {
+            load_kh();
+        }
+
+        private void dt_GridKH_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_MSKH.Text = dt_GridKH.CurrentRow.Cells["MSKH"].Value.ToString();
+            txt_HoTen.Text = dt_GridKH.CurrentRow.Cells["HoTen"].Value.ToString();
+            txt_DiaChi.Text = dt_GridKH.CurrentRow.Cells["DiaChi"].Value.ToString();
+        }
+    }
+}
